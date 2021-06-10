34dd84e844705a0aa177f4d73f18141807905cc9
javtavmon <72968742+javtavmon@users.noreply.github.com>
2021-06-10 16:02:15 +0200

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,10 @@
+setwd("C:/Users/W10/Documents/JAVIER/R/Coursera/Exploratory Data Analysis")
+data_file <- "C:/Users/W10/Documents/JAVIER/R/Coursera/Exploratory Data Analysis/household_power_consumption.txt"
+data <- read.table(data_file, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#str(subSetData)
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
